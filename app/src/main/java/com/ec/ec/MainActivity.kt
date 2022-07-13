19d188de26fa53390a668fd40c07f22934b339e2
shoki <a01050335943@Gmail.com>
2022-07-13 13:33:05 +0900

    load url

--- app/src/main/java/com/ec/ec/MainActivity.kt
+++ app/src/main/java/com/ec/ec/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.ec.ec
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ec.ec.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initWebView()
+        binding.webview.loadUrl("http://sqrt5.iptime.org:8082")
+    }
+
+    private fun initWebView() {
+        with(binding.webview.settings) {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+
+            setSupportMultipleWindows(true)
+            builtInZoomControls = false
+            displayZoomControls = false
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            allowFileAccess = true
+            allowContentAccess = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                binding.webview.onRequestPermissionResult()
+            }
+        }
     }
 }
