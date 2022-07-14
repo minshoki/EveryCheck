a9a080ad7b8667aa54c13e49edd157dbf273d764
shoki <a01050335943@Gmail.com>
2022-07-14 16:33:57 +0900

    test

--- app/src/main/java/com/ec/ec/MainActivity.kt
+++ app/src/main/java/com/ec/ec/MainActivity.kt
@@ -3,18 +3,30 @@ package com.ec.ec
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.ec.ec.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        var instance: MainActivity? = null
+        const val PUSH_BASE_URL = "http://sqrt5.iptime.org:8082"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        instance = this
         initWebView()
-        binding.webview.loadUrl("http://sqrt5.iptime.org:8082")
+        binding.webview.loadUrl(PUSH_BASE_URL)
+    }
+
+    override fun onDestroy() {
+        instance = null
+        super.onDestroy()
     }
 
     private fun initWebView() {
@@ -30,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             allowFileAccess = true
             allowContentAccess = true
         }
+        binding.webview.webViewClient = WebViewClient()
     }
 
     override fun onRequestPermissionsResult(
